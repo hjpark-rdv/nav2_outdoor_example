7cb934f9c2fabb26eab13fb5cc4cbe952594634a
hjpark <hjpark@accupix.com>
2025-09-01 13:19:10 +0900

    ros2 에서는 world(sdf)를 교체하면, 센서 plugin도 함께 다른 world(sdf)파일에 넣어줘야 함. playground를 사용하면 imu, gps등이 올라오지 않았던 이유 확인

--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -31,7 +31,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
         launch_arguments={
-            'gz_args': ' -r ' + pkg_share + '/world/empty.sdf'
+            'gz_args': ' -r ' + pkg_share + '/world/playground.sdf'
         }.items(),
     )
 
